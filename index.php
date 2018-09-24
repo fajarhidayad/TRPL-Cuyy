8997f6af437c6d66410febae5aa327529d9b8a56
fajarhidayad <fajarsuryahidayad@gmail.com>
2018-09-24 11:38:53 +0700

    form login

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+<link rel="stylesheet" type="text/css" href="css/styleIndex.css">
+
+</head>
+<body>
+	<div id="atas">Kerajinan.id</div><br>
+	<div>
+		<ul>
+			<li><a href="" id="navigasi">Home</a></li>
+			<li><a href="" id="navigasi">Toko</a></li>
+			<li><a href="" id="navigasi">Event</a></li>
+			<li><a href="" id="navigasi">Pesanan</a></li>
+			<div id="cari">
+			<input type="" name="" placeholder=Cari>
+			</div>
+			<div id="daftar">
+				<li><a href="login.php" style="color: #333;">Login</a></li>
+				<li><a href="" style="color: #333;">Daftar</a></li>
+			</div>
+			
+		</ul>
+	</div>
+</body>
+</html>
